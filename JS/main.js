ba44ed5088208e9d5040589f55e8abfa540d4774
Guilherme Castro <gui.castro.0001.1426@gmail.com>
2024-09-07 01:51:23 -0300

    10  Commit

--- JS/main.js
+++ JS/main.js
@@ -30,10 +30,10 @@
 // setInterval(Carrossel2, 2000);
 
 const projetos = [
-    { imgSrc: './IMG/2.png', github: '#', site: '#' },
-    { imgSrc: './IMG/3.png', github: '#', site: '#' },
-    { imgSrc: './IMG/1.png', github: '#', site: '#' },
-    { imgSrc: './IMG/COPIAdacopiaRA.jpg', github: '#', site: '#' },
+    { imgSrc: '/IMG/2.png', github: '#', site: '#' },
+    { imgSrc: '/IMG/3.png', github: '#', site: '#' },
+    { imgSrc: '/IMG/1.png', github: '#', site: '#' },
+    { imgSrc: '/IMG/COPIAdacopiaRA.jpg', github: '#', site: '#' },
     
 ];
 
@@ -68,20 +68,20 @@ projetos.forEach(projeto => {
 });
 
 const ArrayDeItems = [
-    { imgSrc: './IMG/HTML.png', h3: 'HTML' },
-    { imgSrc: './IMG/CSS.png', h3: 'CSS' },
-    { imgSrc: './IMG/JS.png', h3: 'JAVASCRIPT' },
-    { imgSrc: './IMG/GIT.png', h3: 'GIT GIT-HUB' },
-    { imgSrc: './IMG/SKILLS.png', h3: 'COMUNICAÇÃO E TRABALHOS EM EQUIPE' },
-    { imgSrc: './IMG/FORMADO.png', h3: 'FORMADO EM DESENVOLVIMENTO WEB' },
-    { imgSrc: './IMG/ESPANHOL.png', h3: 'ESPANHOL INTERMEDIARIO' },
-    { imgSrc: './IMG/WORD.png', h3: 'WORD' },
-    { imgSrc: './IMG/EXCEL.png', h3: 'EXCEL' },
-    { imgSrc: './IMG/PPTX.png', h3: 'POWERPOINT' },
-    { imgSrc: './IMG/MONTAGEM.png', h3: 'MONTAGEM E MANUTENÇÃO PREVENTIVA DE COMPUTADORES' },
-    { imgSrc: './IMG/REDES.png', h3: 'INSTALAÇÃO, CONFIGURAÇÃO E GERENCIAMENTO DO WINDOWS E REDES' },
-    { imgSrc: './IMG/FORMADO02.png', h3: 'FORMADO EM SUPORTE TI' },
-    { imgSrc: './IMG/ESPANHOL.png', h3: 'INGLÊS BÁSICO' }
+    { imgSrc: 'IMG/HTML.png', h3: 'HTML' },
+    { imgSrc: 'IMG/CSS.png', h3: 'CSS' },
+    { imgSrc: 'IMG/JS.png', h3: 'JAVASCRIPT' },
+    { imgSrc: 'IMG/GIT.png', h3: 'GIT GIT-HUB' },
+    { imgSrc: 'IMG/SKILLS.png', h3: 'COMUNICAÇÃO E TRABALHOS EM EQUIPE' },
+    { imgSrc: 'IMG/FORMADO.png', h3: 'FORMADO EM DESENVOLVIMENTO WEB' },
+    { imgSrc: 'IMG/ESPANHOL.png', h3: 'ESPANHOL INTERMEDIARIO' },
+    { imgSrc: 'IMG/WORD.png', h3: 'WORD' },
+    { imgSrc: 'IMG/EXCEL.png', h3: 'EXCEL' },
+    { imgSrc: 'IMG/PPTX.png', h3: 'POWERPOINT' },
+    { imgSrc: 'IMG/MONTAGEM.png', h3: 'MONTAGEM E MANUTENÇÃO PREVENTIVA DE COMPUTADORES' },
+    { imgSrc: 'IMG/REDES.png', h3: 'INSTALAÇÃO, CONFIGURAÇÃO E GERENCIAMENTO DO WINDOWS E REDES' },
+    { imgSrc: 'IMG/FORMADO02.png', h3: 'FORMADO EM SUPORTE TI' },
+    { imgSrc: 'IMG/ESPANHOL.png', h3: 'INGLÊS BÁSICO' }
 ];
 
 const competencias = document.querySelector('.competencias');
